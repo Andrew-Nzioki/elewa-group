c0542a1e7596b523fbbbfb5892be2fdf5482f5af
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2023-11-02 17:34:43 +0300

    :construction:update the navbar to use Links for navigation accross the different routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Layout/Navbar/Navbar";
+
 import InvestPage from "./views/invest/Invest";
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
+     
       <Routes>
         <Route path="/" element={<InvestPage />} />
         <Route path="/social-impact" element={<h1>Social impact</h1>} />
--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="p-28 text-lg text-[#FFFFFF]">
       <div className="flex items-center justify-between">
         <h1>elewa-group</h1>
-        <ul className="flex gap-6  h-10 bg-transparent">
-          <li>Home</li>
-          <li>About us</li>
-          <li>Social Impact</li>
-          <li>Invest</li>
+        <ul className="flex md:flex-row sm:flex-col gap-6  h-10 bg-transparent">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/social-impact">Social Impact</Link>
+          </li>
+          <li>
+            <Link to="/">Invest</Link>
+          </li>
           <li>Venture labs</li>
           <li>Brands</li>
           <li>Careers</li>
