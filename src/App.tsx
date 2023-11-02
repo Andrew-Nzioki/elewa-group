735a36ef444a374cd3a63a409da89e1a9e1c3a08
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2023-11-02 16:04:07 +0300

    :art:Navabr border

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar/Navbar";
 import InvestPage from "./views/invest/Invest";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<InvestPage />} />
-      <Route path="/social-impact" element={<h1>Social impact</h1>} />
-      <Route path="/about" element={<h1>About page</h1>} />
-    </Routes>
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<InvestPage />} />
+        <Route path="/social-impact" element={<h1>Social impact</h1>} />
+        <Route path="/about" element={<h1>About page</h1>} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex flex-col items-center p-16">
+      <div className="flex items-center justify-between px-28 py-8 ">
+        <h1>elewa-group</h1>
+        <ul className="flex gap-4 p-4  h-16 bg-transparent">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Social Impact</li>
+          <li>Invest</li>
+          <li>Venture labs</li>
+          <li>Brands</li>
+          <li>Careers</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <hr className="border-b-2 border-[#FFFFFF] w-full bg-transparent " />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
